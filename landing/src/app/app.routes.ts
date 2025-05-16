a515b90c17feef8520bd09b7a665b0a2d1445805
Jahongir <jahongirakbarov007@gmail.com>
2025-05-16 09:42:33 +0500

    commit 1

--- landing/src/app/app.routes.ts
+++ landing/src/app/app.routes.ts
@@ -1,22 +1,27 @@
 import { Routes } from '@angular/router';
+import LoginComponent from './components/login/login.component';
 
 export const routes: Routes = [
+  // {
+  //   path: "",
+  //   pathMatch: "full",
+  //   redirectTo: "login"
+  // },
+  // {
+  //   path: "",
+  //   children: [
+  //     {
+  //       path: "login",
+  //       loadComponent: ()=> import("./components/login/login.component")
+  //     },
+  //     {
+  //       path: "statistic",
+  //       loadComponent: ()=> import("./components/statistic/statistic.component")
+  //     }
+  //   ]
+  // }
   {
     path: "",
-    pathMatch: "full",
-    redirectTo: "login"
+    component: LoginComponent,
   },
-  {
-    path: "",
-    children: [
-      {
-        path: "login",
-        loadComponent: ()=> import("./components/login/login.component")
-      },
-      {
-        path: "statistic",
-        loadComponent: ()=> import("./components/statistic/statistic.component")
-      }
-    ]
-  }
 ];
